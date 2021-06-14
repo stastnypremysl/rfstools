e4bd13105b4e62334b27ca15333fef741d7b770e
Přemysl Šťastný <p-w@stty.cz>
2021-06-14 17:25:18 +0200

    packages imports fixed

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
 import setuptools
+import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
   long_description = fh.read()
 
   setuptools.setup(name='rfstools',
-    version='0.1.0',
+    version='0.1.1',
     description='Remote file system tools for data manipulation between remote /and local host/ (cp, mv, ls, rm,..)',
     long_description=long_description,
     author='Přemysl Šťastný',
     author_email='p-w@stty.cz',
     url='https://git.profinit.eu/pstastny/rfstools',
     package_dir={"": "src"},
+    packages=['_rfstools'],
     install_requires=[
       'rfslib>=0,<1',
       'ConfigArgParse>=1,<2'
-    ]
+    ],
+    scripts=[*map(lambda x: 'bin/' + x, os.listdir('bin'))],
   )

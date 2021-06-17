118fe09bce628feb794586ef445633a647e65bf4
Přemysl Šťastný <p-w@stty.cz>
2021-06-17 10:57:38 +0200

    new version 0.3.3

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git pull
+
+new_version=$(cat ./version.txt)
+if git tag | grep -xF "$new_version" >/dev/null; then
+  echo "This version have been already deployed."
+  exit 1
+fi
+
+git add -A
+git commit -m "new version $new_version"
+git tag $(cat ./version.txt)
+git push --tags

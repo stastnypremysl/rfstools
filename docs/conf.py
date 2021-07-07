9a50629621f6fa0047ed0c2dff8b77365bb63b76
Přemysl Šťastný <p-w@stty.cz>
2021-07-07 07:50:54 +0200

    removed docs generation bug

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,7 @@ author = 'Přemysl Šťastný'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx_markdown_builder',
-'sphinx.ext.napoleon',
-]
+extensions = ['sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
